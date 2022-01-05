cb033d1b743182bdb273c12103f9ae2b868a3baf
kamruzzaman leeon <leeon.ewubd@gmail.com>
2022-01-05 10:05:05 +0600

    Update logout.php

--- lab8/logout.php
+++ lab8/logout.php
@@ -1,7 +1,8 @@
 <?php
 
 session_start();
-setcookie(session_name(), "", time() - 3600*24*30);
+setcookie('id', "", time() - 3600*24*30);
+setcookie('password', "", time() - 3600*24*30);
  //send browser command remove sid from cookie
 session_destroy();
  //remove sid-login from server storage
